8a9d1143c6bce0cd62f1bebdb921fa7aa0c09a30
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2023-08-23 11:46:04 -0600

    can save timer sets, create timers, name timers

--- workout-interval-timer-expo-app/app/index.js
+++ workout-interval-timer-expo-app/app/index.js
@@ -1,9 +1,11 @@
 import { View, Text }  from 'react-native';
+import WorkoutTimer from '../components/workouttimer/WorkoutTimer';
 
 const Home = () => {
     return (
         <View>
             <Text>Home</Text>
+            <WorkoutTimer/>
         </View>
     )
     
--- workout-interval-timer-expo-app/components/workouttimer/WorkoutTimer.jsx
+++ workout-interval-timer-expo-app/components/workouttimer/WorkoutTimer.jsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, FlatList } from 'react-native';
 
 const WorkoutTimer = () => {
-  const [seconds, setSeconds] = useState(0);
+  const [timers, setTimers] = useState([]);
+  const [timerName, setTimerName] = useState('');
+  const [presetName, setPresetName] = useState('');
   const [isRunning, setIsRunning] = useState(false);
+  const [presets, setPresets] = useState([]);
 
   useEffect(() => {
-    let timerId;
+    let interval;
 
     if (isRunning) {
-      timerId = setInterval(() => {
-        setSeconds(prevSeconds => prevSeconds + 1);
+      interval = setInterval(() => {
+        setTimers(prevTimers =>
+          prevTimers.map(timer =>
+            timer.seconds > 0 ? { ...timer, seconds: timer.seconds - 1 } : timer
+          )
+        );
       }, 1000);
     } else {
-      clearInterval(timerId);
+      clearInterval(interval);
     }
 
     return () => {
-      clearInterval(timerId);
+      clearInterval(interval);
     };
   }, [isRunning]);
 
@@ -26,15 +33,54 @@ const WorkoutTimer = () => {
   };
 
   const resetTimer = () => {
-    setSeconds(0);
+    setTimers([]);
     setIsRunning(false);
+  };
+
+  const addTimer = (seconds) => {
+    setTimers(prevTimers => [...prevTimers, { name: timerName, seconds }]);
+    setTimerName('');
+  };
+
+  const savePreset = () => {
+    const newPreset = { name: presetName, timers: [...timers] };
+    setPresets(prevPresets => [...prevPresets, newPreset]);
+    setPresetName('');
   };
 
   return (
     <View>
-      <Text>{seconds} seconds</Text>
+      {timers.map((timer, index) => (
+        <Text key={index}>
+          {timer.name} - {timer.seconds} seconds
+        </Text>
+      ))}
+      <TextInput
+        placeholder="Timer Name"
+        value={timerName}
+        onChangeText={text => setTimerName(text)}
+      />
       <Button title={isRunning ? 'Pause' : 'Start'} onPress={toggleTimer} />
       <Button title="Reset" onPress={resetTimer} />
+      <Button title="Add Timer (30 seconds)" onPress={() => addTimer(30)} />
+
+      <TextInput
+        placeholder="Preset Name"
+        value={presetName}
+        onChangeText={text => setPresetName(text)}
+      />
+      <Button title="Save Preset" onPress={savePreset} />
+
+      <FlatList
+        data={presets}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Button
+            title={item.name}
+            onPress={() => setTimers(item.timers)}
+          />
+        )}
+      />
     </View>
   );
 };
